8b474709074f752a9b3bde5bb02c0f999f305ace
appozofx <arturo2pozo@gmail.com>
2026-04-08 20:36:30 -0500

    cambieo en tiempo de importacion

--- firebase/functions/src/winboScheduler.ts
+++ firebase/functions/src/winboScheduler.ts
@@ -28,7 +28,7 @@ function isWithinWindowLima(scheduleDate: Date) {
 export const winboOrdenesAutoSync = onSchedule(
   {
     region: "us-central1",
-    schedule: "every 15 minutes",
+    schedule: "every 5 minutes",
     timeZone: "America/Lima",
     secrets: [WINBO_CRON_TOKEN],
   },
